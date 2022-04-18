e38cbf6f1e637bcd17ef55d26a294950f3821f91
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-18 08:37:53 -0600

    Update: service specs

--- spec/services/direction_service_spec.rb
+++ spec/services/direction_service_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'get directions to charging station', type: :service do
+RSpec.describe 'Direction Service', type: :service do
 
   it 'gets directions, distance and travel time' do
-    directions = DirectionService.get_directions('1331 17th St, Denver, CO', '1225 17th Street, Suite 130, Denver, CO')
+    trip = DirectionService.get_directions('1331 17th St, Denver, CO', '1225 17th Street, Suite 130, Denver, CO')
 
-    binding.pry
+    distance = trip[:route][:legs].sum do |leg|
+      leg[:maneuvers].sum do |maneuver|
+        maneuver[:distance]
+      end
+    end.round(1)
+
+    travel_time = (trip[:route][:legs].sum do |leg|
+      leg[:maneuvers].sum do |maneuver|
+        maneuver[:time]
+      end
+    end.to_f / 60).ceil
+
+    directions = trip[:route][:legs].map do |leg|
+      leg[:maneuvers].map do |maneuver|
+        maneuver[:narrative]
+      end
+    end.flatten.join(" ")
+
+    expect(distance).to eq(0.1)
+    expect(travel_time).to eq(1)
+    expect(directions).to eq("Start out going southeast on 17th St toward Larimer St/CO-33. 1225 17TH ST, SUITE 130 is on the right.")
   end
 end
--- spec/services/station_service_spec.rb
+++ spec/services/station_service_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'search for nearest charging station', type: :service do
+RSpec.describe 'Station Service', type: :service do
 
   it 'gets nearest station data' do
     station = StationService.find_nearest('1331 17th St, Denver, CO')
 
     info = station[:fuel_stations][0]
+    address = [info[:street_address], info[:city], info[:state]].join(", ")
 
     expect(info[:station_name]).to eq("CBRE As Agent for EQC Operating Trust")
-    expect(info[:street_address]).to eq("1225 17th Street, Suite 130, Denver, CO")
+    expect(address).to eq("1225 17th Street, Suite 130, Denver, CO")
     expect(info[:fuel_type_code]).to eq("ELEC")
     expect(info[:access_days_time]).to eq("MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified")
   end
